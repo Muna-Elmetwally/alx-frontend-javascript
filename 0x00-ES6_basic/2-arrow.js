403abb318bd728c14a8a1c48a1636a091f63c755
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 02:26:26 +0200

    .

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,12 @@
-export default function getNeighborhoodsList() {
+export default class getNeighborhoodsList {
+  constructor() {
     this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
+
     const self = this;
     this.addNeighborhood = (newNeighborhood) => {
       self.sanFranciscoNeighborhoods.push(newNeighborhood);
       return self.sanFranciscoNeighborhoods;
     };
   }
+}
   
