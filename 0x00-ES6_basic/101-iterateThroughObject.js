cbfbeb897370263b2390d86476cffd1aee75d68c
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 03:34:08 +0200

    .

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,14 @@
+/* eslint-disable radix */
+export default function iterateThroughObject(reportWithIterator) {
+    let employeeNames = '';
+    for (const [index, item] of Object.entries(reportWithIterator)) {
+      employeeNames += `${item}`;
+  
+      if (parseInt(index) !== reportWithIterator.length - 1) {
+        employeeNames += ' | ';
+      }
+    }
+  
+    return employeeNames;
+  }
+  
