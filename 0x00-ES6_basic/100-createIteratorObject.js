197008d1dd4316ed5e9e62a81aff2dd4deb7cad5
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 06:12:13 +0300

    .

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,4 @@
-export default function createIteratorObject(report) {
+export function createIteratorObject(report) {
   const myarray = [];
   for (const item of Object.values(report.allEmployees)) {
     myarray.push(...item);
