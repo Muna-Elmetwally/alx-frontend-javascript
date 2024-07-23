5adaa431105ac926295ecddf1ccd5a1f0332f826
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 06:48:16 +0300

    .

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-export default function concatArrays(array1, array2, string) {
+export function concatArrays(array1, array2, string) {
   return [...array1, ...array2, ...string];
 }
