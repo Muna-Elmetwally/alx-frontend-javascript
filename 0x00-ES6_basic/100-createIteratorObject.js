7d9e5b8236efd835af4173a4c5fead81f366861e
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 06:54:39 +0300

    .

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,4 +1,5 @@
-export default function createIteratorObject(report) {
+/* eslint-disable import/prefer-default-export */
+export function createIteratorObject(report) {
   const myarray = [];
   for (const item of Object.values(report.allEmployees)) {
     myarray.push(...item);
