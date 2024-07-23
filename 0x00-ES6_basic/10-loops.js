ca17f9aeec45fc6e604b47834b8071ea0a4b8af7
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 06:52:11 +0300

    .

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  // eslint-disable-next-line
-    for (const value of array) {
-    // eslint-disable-next-line
-      array[array.indexOf(value)] = appendString + value;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
 
-  return array;
+  return newArray;
 }
