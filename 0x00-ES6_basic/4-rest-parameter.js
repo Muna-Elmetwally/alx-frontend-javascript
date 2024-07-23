da82954f1c5fcf3e732b98be26d642ef24651c46
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-23 05:37:02 +0300

    .

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export default function returnHowManyArguments(...params) {
-    let numOfParams = 0;
-    for (const _param of params) numOfParams += 1;
-    return numOfParams;
+  let numOfParams = 0;
+  for (const _param of params) numOfParams += 1;
+  return numOfParams;
   //   return params.length;
-  }
+}
