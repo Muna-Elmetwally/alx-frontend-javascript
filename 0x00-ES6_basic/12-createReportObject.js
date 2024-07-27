c1de52a420a4882c3e803c610a3e33518c3e11c6
Muna Elmetwally <munaelmetwally2397@gmail.com>
2024-07-28 01:21:04 +0300

    .

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,12 +1,8 @@
 export default function createReportObject(employeesList) {
-  const employeesObj = {
-    allEmployees: {
-      ...employeesList,
-    },
-    getNumberOfDepartments() {
-      const listLength = Object.keys(employeesList).length;
-      return listLength;
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
-  return employeesObj;
 }
